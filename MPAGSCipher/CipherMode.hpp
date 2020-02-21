7589d051a1914d9ee7526fcac64de061a604a0f9
mjs708@student.bham.ac.uk <MJS708@student.bham.ac.uk>
2020-02-21 17:07:14 +0000

    Enumerator header file for choosing to encrypt or decrypt

---
+++ MPAGSCipher/CipherMode.hpp
@@ -0,0 +1,9 @@
+#ifndef MPAGSCIPHER_CipherMode_HPP
+#define MPAGSCIPHER_CipherMode_HPP
+
+enum class Cipher { //Enumerator for whether to encrypt or decrpyt cipher
+  encrypt,
+  decrypt
+};
+
+#endif
